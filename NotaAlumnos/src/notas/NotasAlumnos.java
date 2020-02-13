8ae47c1a28e2d38e0a935634488daf6f6b4e32f1
Alvaro <alvaro030400@gmail.com>
2020-02-13 13:24:18 +0100

    sladñfhañl
    Signed-off-by: Alvaro <alvaro030400@gmail.com>

--- NotaAlumnos/src/notas/NotasAlumnos.java
+++ NotaAlumnos/src/notas/NotasAlumnos.java
@@ -12,6 +12,7 @@ public class NotasAlumnos {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Holasaasadddds");
+		System.out.println("Holaaaa");
 			
 	}
 
