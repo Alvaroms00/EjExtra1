5586440318eda8faba36b50f3bf6a1676fbbd8db
Alvaro <alvaro030400@gmail.com>
2020-02-13 12:31:12 +0100

    Primera version
    Crear codigo y comentar.
    
    Signed-off-by: Alvaro <alvaro030400@gmail.com>

---
+++ NotaAlumnos/src/notas/NotasAlumnos.java
@@ -0,0 +1,17 @@
+package notas;
+/**
+ * <h1>Clase notas de alumnos</h1>
+ * @author Alvaro
+ * @version 1.0
+ * 
+ */
+
+public class NotasAlumnos {
+	
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+			System.out.println("hola");
+	}
+
+}
